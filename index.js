a50390839a38989163e2bcafd542c4bcc87d099f
Kirill <kirillplusena@gmail.com>
2021-03-19 16:32:47 +0300

    refactor: correct code

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const video = document.getElementById('video');
 const progress = document.getElementById('progress');
 const progressBar = document.getElementById('progress-line');
 const ADDED_INDENTATION = 2;
+const min = 0.4;
+const max = 4;
 function changeSpeed(e) {
   const coordinateY = e.pageY - this.offsetTop + ADDED_INDENTATION;
   const persent = coordinateY / this.offsetHeight;
-  const heigth = Math.round(persent * 100) + '%';
-  const min = 0.4;
-  const max = 4;
+  const height = Math.round(persent * 100) + '%';
   const resultsPlaybackRate = persent * (max - min) + min;
   const optimizePlaybackRate = resultsPlaybackRate > max ? max : resultsPlaybackRate;
   progressBar.innerHTML = optimizePlaybackRate.toFixed(2) + 'x';
   video.playbackRate = optimizePlaybackRate;
-  progressBar.style.height = heigth;
+  progressBar.style.height = height;
 }
 
 window.onload = () => {
